158e73ab40d42ebd9023d792ab6d163cede889cf
murata <murata@kmc.gr.jp>
2017-04-23 05:00:16 +0900

    micro keybind

--- deploy.sh
+++ deploy.sh
@@ -8,9 +8,14 @@ for f in .*;do
     fi
 done
 
+mkdir -p ~/.config/micro
+ln -s ~/dotfiles/.config/micro/bindings.json ~/.config/micro/bindings.json
+
 if [ ! -e ~/.zsh ];then
   mkdir ~/.zsh
 fi
 git clone https://github.com/zsh-users/zsh-syntax-highlighting ~/.zsh/zsh-syntax-highlighting
 git clone https://github.com/seebi/dircolors-solarized ~/.zsh/dircolors-solarized
 git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosuggestions
+
+
