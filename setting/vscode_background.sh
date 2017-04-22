9b2c7b984973e779bf28414881b706bc76a9493d
murata <murata@kmc.gr.jp>
2017-04-23 03:56:10 +0900

    vscode backgorund

---
+++ setting/vscode_background.sh
@@ -0,0 +1,5 @@
+echo ".composite.panel.integrated-terminal{  background-image: url('/Users/m
+urata/Pictures/background/sagisawa.jpg') background-position: center center; bac
+kground-repeat: no-repeat;background-attachment: fixed;  background-size: cover;
+ }" > /Applications/Visual\ Studio\ Code.app/Contents/Resources/app/out/vs/workb
+ench/electron-browser/workbench.main.css
