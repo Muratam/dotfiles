40eb6d105af3c30446b762f400948c44746af366
murata <murata@kmc.gr.jp>
2017-05-13 20:16:37 +0900

    fi

--- lib/dl72.py
+++ lib/dl72.py
@@ -2,8 +2,6 @@ from bs4 import BeautifulSoup
 import requests
 import os
 import shutil
-from pprint import pprint
-import re
 
 
 class DL72:
@@ -14,37 +12,28 @@ class DL72:
         self.html = self.got.text
         self.soup = BeautifulSoup(self.html, "html5lib")
 
-    def _make_local_path(url, dirname):
+    def _make_local_path(url, dirname, local_prefix):
         if not dirname:
             return os.path.basename(url)
         if not dirname.endswith("/"):
             dirname += "/"
         if not os.path.exists(dirname):
             os.mkdir(dirname)
-        return dirname + os.path.basename(url)
+        return dirname + str(local_prefix) + os.path.basename(url)
 
-    def dl_binary(url, dirname=""):
-        local_path = DL72._make_local_path(url, dirname)
-        with open(local_path, "wb") as f:
-            shutil.copyfileobj(requests.get(url, stream=True).raw, f)
-
-    def dl(url, dirname=""):
-        local_path = DL72._make_local_path(url, dirname)
-        with open(local_path, "w") as f:
-            f.write(requests.get(url).text)
+    def download(url, dirname="", local_prefix="", binary=True):
+        local_path = DL72._make_local_path(url, dirname, local_prefix)
+        if binary:
+            with open(local_path, "wb") as f:
+                shutil.copyfileobj(requests.get(url, stream=True).raw, f)
+        else:
+            with open(local_path, "w") as f:
+                f.write(requests.get(url).text)
 
 if __name__ == "__main__":
-    basepath = "http://people.cs.ubc.ca/~murphyk/MLbook/figReport-16-Aug-2012/"
-    soup = DL72(basepath + "pmlFigureCodeTable.html").soup
-    pdfs = []
-    for tr in soup.select("tr"):
-        tds = tr.select("td")
-        if len(tds) <= 2:
-            continue
-        if not tds[0].text.startswith("8"):
-            continue
-        for a in tds[1].select("a"):
-            pdfs.append(a.get("href"))
-    for pdf in pdfs:
-        print(pdf)
-        DL72.dl_binary(basepath + pdf, "mlapp")
+    print("""Usage :
+    soup = DL72("http://example.com").soup
+    for a in soup.select("a"):
+        if a.text.startswith("aaa"):
+            print(a)
+    DL72.download("http://example.com/hoge.jpg","hoge",0)""")
