04cee4a8f81ac31b036f8fa71f91a7191a7c88d4
murata <murata@kmc.gr.jp>
2017-06-28 03:04:42 +0900

    vim initsh

--- init.sh
+++ init.sh
@@ -9,7 +9,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions.git ~/.zsh/zsh-autosu
 
 
 # vim
-# plug_file="${HOME}/.vim/autoload/plug.vim"
-# if [[ ! -f $plug_file ]];then
-#   curl -Lo $plug_file --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-# fi
+plug_file="${HOME}/.vim/autoload/plug.vim"
+if [[ ! -f $plug_file ]];then
+  curl -Lo $plug_file --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+fi
