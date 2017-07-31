e720c4d33ded1a22b72da3932af1491f339cc0cf
murata <murata@kmc.gr.jp>
2017-08-01 03:23:23 +0900

    nimble

--- home/.bashrc_languages.sh
+++ home/.bashrc_languages.sh
@@ -4,30 +4,42 @@ export-path ~/codes/dlang/workspace-d/bin
 # nim
 if [[ -x "$(command -v nim)" ]]; then
   nimcompile(){ nim c -d:release --hints:off --verbosity:0 $@ ; } # for compile
-  alias nimr="nimcompile -r" # run
-  alias nimr++="nimcompile -r -d:ssl" # all in one
+  alias nimr-pure="nimcompile -r" # pure
+  nimr(){
+    exename=`echo $1 | sed 's/\.[^\.]*$//'`
+    [[ -f "nimcache/$exename" ]] && mv nimcache/$exename ./
+    nimcompile -r -d:ssl $1
+    [[ -f $exename ]] && [[ -d "nimcache" ]] && mv $exename nimcache
+  }
+  export-path ~/.nimble/bin/
+  #alias nimr="nimcompile -r -d:ssl" # all in one
 fi
 
-# for yukicoder :: DOWNLOAD No.$(basename $(pwd)) && TEST a.out
-yukioj(){
-  if [[ ! -d test ]]; then
-    if [[ "$1" == "" ]]; then # no problem number
-      oj dl "https://yukicoder.me/problems/no/$(basename $(pwd))"
-    else
-      oj dl "https://yukicoder.me/problems/no/$1"
-    fi
-  fi
-  oj test
-}
 # /**/5 , *.nim => test yukicoder
 nimoj(){
+  yukioj(){
+    # for yukicoder :: DOWNLOAD No.$(basename $(pwd)) && TEST a.out
+    if [[ ! -d test ]]; then
+      if [[ "$1" == "" ]]; then # no problem number
+        oj dl "https://yukicoder.me/problems/no/$(basename $(pwd))"
+      else
+        oj dl "https://yukicoder.me/problems/no/$1"
+      fi
+    fi
+    oj test
+  }
   if [[ "$1" == "" ]]; then # no filename
     nimcompile -o:a.out *.nim
+    [[ $? != 0 ]] && return $?
   elif [[ "$1" == "clean" ]]; then
     rm -rf test a.out nimcache
     return 0
+  elif [[ "$1" == "copy" ]]; then
+    cat *.nim | pbcopy
+    return 0
   else
     nimcompile -o:a.out $@
+    [[ $? != 0 ]] && return $?
   fi
   echo "[[ a.out ]]"
   yukioj
