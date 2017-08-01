e150f8674ecea056688261c31969d01a78cd59c6
murata <murata@kmc.gr.jp>
2017-08-01 10:16:48 +0900

    transpose

--- home/.nimble/pkgs/userimport/templates.nim
+++ home/.nimble/pkgs/userimport/templates.nim
@@ -1,11 +1,11 @@
 import sequtils,strutils,strscans,algorithm,math,future,sets,queues,tables,macros
+macro unpack*(rhs: seq,cnt: static[int]): auto =
+  let t = genSym(); result = quote do:(let `t` = `rhs`;())
+  for i in 0..<cnt: result[0][1].add(quote do:`t`[`i`])
 template get*():string = stdin.readLine()
 template times*(n:int,body:untyped): untyped = (for _ in 0..<n: body)
 template `max=`*(x,y:typed):void = x = max(x,y)
 template `min=`*(x,y:typed):void = x = min(x,y)
-macro unpack(rhs: seq,cnt: static[int]): auto =
-  let t = genSym(); result = quote do:(let `t` = `rhs`;())
-  for i in 0..<cnt: result[0][1].add(quote do:`t`[`i`])
 
 # future: dump / lc[(x,y,z) | (x <- 1..n, y <- x..n, z <- y..n, x*x + y*y == z*z),tuple[a,b,c: int]]
 ### template assignOperators():untyped =
@@ -34,3 +34,10 @@ template eachIt*[T](arr:var seq[T],body:untyped):untyped =
   for i in 0..<arr.len:(var it{.inject.}=arr[i]; body; defer:arr[i]=it)
 template eachIt*[T](arr:var seq[T],i,body:untyped):untyped =
   for i in 0..<arr.len:(var it{.inject.}=arr[i]; body; defer:arr[i]=it)
+
+
+# matrix library
+proc transpose*[T](mat:seq[seq[T]]):seq[seq[T]] =
+  result = newSeqWith(mat[0].len,newSeq[T](mat.len))
+  for x,xs in mat: (for y,ys in xs:result[y][x] = mat[x][y])
+
