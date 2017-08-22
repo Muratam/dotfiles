789a854ef88c45c6c023c592d61f2f8c4e126664
murata <murata@kmc.gr.jp>
2017-08-22 09:52:06 +0900

    init

--- init.sh
+++ init.sh
@@ -27,3 +27,5 @@ plug_file="${HOME}/.vim/autoload/plug.vim"
 if [[ ! -f $plug_file ]];then
   curl -Lo $plug_file --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+
+${HOME}/.homesick/repos/homeshick/bin/homeshick link
