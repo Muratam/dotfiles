b39551c8a789edc7417169b610b4d18eebb8c48d
murata <murata@kmc.gr.jp>
2017-08-31 19:03:51 +0900

    minimal recomend

--- recommend.sh
+++ recommend.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 # iikanji command lists
 declare recommendeds=( git tmux zsh vim tig tree wget unzip less )
-declare convinients=( jq rlwrap highlight thefuck sshfs )
+declare convinients=( highlight )
 execable(){ [[ -x "$(command -v $1)" ]] || [[ "$(command -v $1)" != "" ]] ; }
 green(){ echo "`tput setaf 2`$@`tput sgr0`"; }
 yellow(){ echo "`tput setaf 3`$@`tput sgr0`"; }
