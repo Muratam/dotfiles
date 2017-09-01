5671ac2636bd9118e5229e1227303739e4e6932c
murata <murata@kmc.gr.jp>
2017-09-01 11:53:06 +0900

    recommend other os

--- recommend.sh
+++ recommend.sh
@@ -25,14 +25,18 @@ fi
 
 # get package manager
 if execable brew; then manager="brew install"
-elif execable apt; then manager="sudo apt install -y"
 elif execable yum; then
   manager="sudo yum install -y";
   sudo yum install -y epel-release
   sudo yum update -y vim-minimal
-elif execable pacman; then manager="sudo pacman -S --noconfirm "
+elif execable apt   ; then manager="sudo apt install -y"
+elif execable pacman; then manager="sudo pacman -S --noconfirm" # ArchLinux
+elif execable zypper; then manager="sudo zypper install -y" # OpenSUSE
+elif execable pkg   ; then manager="sudo pkg install -y" # FreeBSD
 else echo "no package manager"; exit 1
 fi
+
+
 
 # install all recommended commands
 installlist ${recommendeds[@]}
